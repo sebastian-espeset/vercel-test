01a004e402eb2c054030467569f3c531e39f8b37
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-01-28 12:23:52 -0600

    test

---
+++ src/ButtonTest.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export default function ButtonTest() {
+    const [alert, setAlert]= useState(false)
+    const handleWarningClick=(e)=>{
+        e.preventDefault();
+        setAlert(!alert);
+    }
+    if(alert===true){
+        return(
+            <>
+        <h1>I asked you not to click that</h1>
+        <button onClick={handleWarningClick}>Please do not click THIS button...</button>
+        </>
+        )
+    }
+    return (
+        <div>
+            <button onClick={handleWarningClick}>Do not click this button</button>
+
+        </div>
+    )
+}
