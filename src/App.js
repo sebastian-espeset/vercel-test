846f00ab1aa3d54c2e2aedb13289d72636324823
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-01-28 12:57:37 -0600

    background and some functionality changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './App.css';
 import ButtonTest from './ButtonTest';
 
+
+
 function App() {
+  const [alert, setAlert]= useState(false)
+    const handleWarningClick=(e)=>{
+        e.preventDefault();
+        setAlert(!alert);
+    }
+  if(alert===true){
+    return(
+      <main>
+        <h1>I asked you not to click that...</h1>
+        <button onClick={handleWarningClick}>Please do not click THIS button...</button>
+      </main>
+    )
+  }
   
   return (
     <main>
       <h1>This is my site</h1>
-      <ButtonTest/>
+      <ButtonTest handleWarningClick={handleWarningClick} alert={alert}/>
     </main>
   );
 }
--- src/ButtonTest.js
+++ src/ButtonTest.js
@@ -1,24 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-export default function ButtonTest() {
-    const [alert, setAlert]= useState(false)
-    const handleWarningClick=(e)=>{
-        e.preventDefault();
-        setAlert(!alert);
-    }
-    console.log("button test")
-    if(alert===true){
-        return(
-            <>
-        <h1>I asked you not to click that</h1>
-        <button onClick={handleWarningClick}>Please do not click THIS button...</button>
-        </>
-        )
-    }
+export default function ButtonTest({handleWarningClick, alert}) {
     return (
-        <div>
+        <>
             <button onClick={handleWarningClick}>Do not click this button</button>
-        </div>
+        </>
     )
 }
 
